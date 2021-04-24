cc76599464724dfa6a9a8da7adb336f838e14fd8
Santiago Yepes <santiagoyepescarrera@gmail.com>
2021-04-24 17:00:45 -0400

    Added error functionality

--- goal-list-app/src/components/UI/Button/Button.js
+++ goal-list-app/src/components/UI/Button/Button.js
@@ -1,10 +1,9 @@
-import React from 'react';
+import React from "react";
+import styles from "./Button.module.css";
 
-import './Button.css';
-
-const Button = props => {
+const Button = (props) => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
       {props.children}
     </button>
   );
