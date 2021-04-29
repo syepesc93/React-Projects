367702b16820ff3791c1439327008461c252bab6
Santiago Yepes <santiagoyepescarrera@gmail.com>
2021-04-29 17:05:24 -0400

    Added some components

---
+++ food-order-app/src/components/Layout/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = () => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+      </header>
+
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="Delicious foods!" />
+      </div>
+    </> // end of fragment
+  );
+};
+
+export default Header;
